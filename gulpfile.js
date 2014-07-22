c9c8f4729b0170d304a9d407646be00a48be1a85
Thierry Lagasse [OSX] <akarun@krkn.be>
2014-07-22 17:51:27 +0200

    Add "Jade" as templates engine.

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 var gulp = require( 'gulp' );
 var stylus = require( 'gulp-stylus' );
+var jade = require('gulp-jade');
 var minifyCSS = require( 'gulp-minify-css' );
 var koutoSwiss = require( 'kouto-swiss' );
 
@@ -16,15 +17,23 @@ gulp.task( 'stylus', function() {
 
 // ----------------------------------------------------------------------------
 gulp.task( 'html', function() {
-    return gulp.src( './src/*.html')
-        .pipe(gulp.dest('./bin/'));
+    return gulp.src( './src/*.html' )
+        .pipe( gulp.dest( './bin/' ) );
+});
+
+// ----------------------------------------------------------------------------
+gulp.task( 'jade', function() {
+    console.log("Update jade file")
+    return gulp.src( './src/templates/*.jade' )
+        .pipe( jade() )
+        .pipe( gulp.dest( './bin/' ) )
 });
 
 // ----------------------------------------------------------------------------
 gulp.task( 'watch' , function() {
-    gulp.watch( './src/styles/*.styl', ['stylus'] );
-    gulp.watch( './src/*.html', ['html'] );
+    gulp.watch( 'src/styles/*.styl', ['stylus'] );
+    gulp.watch( 'src/templates/*.jade', ['jade'] );
 });
 
 // ----------------------------------------------------------------------------
-gulp.task( 'default', ['stylus', 'html'] );
+gulp.task( 'default', ['stylus', 'watch'] );
