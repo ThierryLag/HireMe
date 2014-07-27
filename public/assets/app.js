5c4754d3213a75e788c11eda781c4a32c662d6f3
Thierry Lagasse [OSX] <akarun@krkn.be>
2014-07-27 18:36:25 +0200

    Improve "Analytics" for outgoing events.

--- public/assets/app.js
+++ public/assets/app.js
@@ -1,30 +1,29 @@
-/**
+
+/*
  *  Passtech.be - HireMe page - Google Analytics Code
  */
-"use strict"
 
-
-// ----------------------------------------------------------------------------
-
-// Default GA code
-(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-ga('create', 'UA-15392844-5', 'auto');
-ga('send', 'pageview');
-
-// ----------------------------------------------------------------------------
-
-$(document).ready(function() { 
-    $( 'a[rel="external"]' ).each(function() {
+(function() {
+  "use strict";
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','http://www.google-analytics.com/analytics.js','ga');
+  (function($) {
+    ga('create', 'UA-15392844-5', 'auto');
+    ga('send', 'pageview');
+    return $(function() {
+      return $('a[rel="external"]').on('click', function(event) {
+        var externalUrl;
         event.preventDefault();
-
-        ga('send', 'event', 'outbound', 'click', $( this ).attr( "href" ), {
-            'hitCallback': function () {
-                document.location = $( this ).attr( "href" );
-            }
+        externalUrl = $(this).attr("href");
+        return ga('send', 'event', 'outbound', 'click', externalUrl, {
+          'hitCallback': function() {
+            return document.location = externalUrl;
+          }
         });
+      });
     });
-});
+  })(jQuery);
+
+}).call(this);
